ae530062033a3a3ab5f9d87f47ed63b2145ccb50
kujenga <ataylor0123@gmail.com>
2014-06-30 22:48:12 -0400

    added nutch crawler file

---
+++ nutch/BasicNutchCrawler.java
@@ -0,0 +1,11 @@
+// Basic Crawler using the Apache Nutch libraries
+
+import org.apache.nutch.crawl;
+
+public class BasicNutchCrawler {
+
+    public static void main(String[] args) {
+	
+    }
+
+}
